91bb74e3f41158ca7c50688c1882d014843745b9
Xavier Riera Carrion <xriera-c@student.hive.fi>
2023-11-30 10:46:40 +0200

    Change library dependency to libft

--- ft_utoa.c
+++ ft_utoa.c
@@ -6,11 +6,11 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 11:15:57 by xriera-c          #+#    #+#             */
-/*   Updated: 2023/11/28 17:27:43 by xriera-c         ###   ########.fr       */
+/*   Updated: 2023/11/30 10:37:49 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "libft/libft.h"
 
 char	*ft_utoa(unsigned int n)
 {
